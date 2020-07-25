74a4c08245e5435ab1fbc6a9a20e3a00fa30d6c4
Tom Johnson <tom@tmjohnson.co.uk>
2020-07-25 17:16:29 +0100

    Commit modifications to JS script

---
+++ assets/js/script.js
@@ -0,0 +1,82 @@
+(function($) {
+  "use strict";
+
+  // navigation
+  $(window).scroll(function() {
+    if ($(".navigation").offset().top > 1) {
+      $(".navigation").addClass("nav-bg");
+    } else {
+      $(".navigation").removeClass("nav-bg");
+    }
+  });
+
+  // video modal popup
+  var $videoSrc;
+  $(".video-modal").click(function() {
+    $videoSrc = $(this).data("src");
+  });
+  $("#videoModal").on("shown.bs.modal", function(e) {
+    $("#video").attr(
+      "src",
+      $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0"
+    );
+  });
+  $("#videoModal").on("hide.bs.modal", function(e) {
+    $("#video").attr("src", $videoSrc);
+  });
+  $("#videoModal2").on("shown.bs.modal", function(e) {
+    $("#video2").attr(
+      "src",
+      $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0"
+    );
+  });
+  $("#videoModal2").on("hide.bs.modal", function(e) {
+    $("#video2").attr("src", $videoSrc);
+  });
+
+  // testimonial slider
+  $(".testimonial-slider").slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: true,
+    prevArrow:
+      "<button type='button' class='prevArrow'><i class='ti-arrow-left'></i></button>",
+    nextArrow:
+      "<button type='button' class='nextArrow'><i class='ti-arrow-right'></i></button>",
+    autoplay: true,
+    responsive: [
+      {
+        breakpoint: 1600,
+        settings: {
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+
+  // Accordions
+  $(".collapse")
+    .on("shown.bs.collapse", function() {
+      $(this)
+        .parent()
+        .find(".ti-plus")
+        .removeClass("ti-plus")
+        .addClass("ti-minus");
+    })
+    .on("hidden.bs.collapse", function() {
+      $(this)
+        .parent()
+        .find(".ti-minus")
+        .removeClass("ti-minus")
+        .addClass("ti-plus");
+    });
+})(jQuery);
